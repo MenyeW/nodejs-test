ef79bed801246b7ed86728ce7338090fdf6202fb
Rahul Bhatnagar <rahul.bhatnagar@paytm.com>
2018-12-05 17:56:11 +0530

    Making a semblance of a website

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,21 @@
+const fs = require('fs');
+const express = require('express');
+var app = express();
 
 module.exports.route = (app) => {
-	app.get('/*', (req,res,next)=>{ res.send ('Hello World!');});
+	app.use(header);
+	app.get('/', (req,res,next)=>{res.write('Base Page'); next();});
+	app.use(footer);
+}
+
+function header(req,res,next){
+	html = fs.readFileSync(__dirname+"/../common/header.html",'utf8');
+	res.write(html);
+	next();
+}
+
+function footer(req,res,next){
+	html = fs.readFileSync(__dirname+"/../common/footer.html",'utf8');
+	res.write(html);
+	res.end();
 }
