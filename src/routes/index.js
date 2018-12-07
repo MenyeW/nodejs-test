47052a11f09828ff50ab4ca2e0daa7645874930d
Rahul Bhatnagar <rahul.bhatnagar@paytm.com>
2018-12-07 11:30:26 +0530

    Added some views

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,42 @@
 const fs = require('fs');
 const express = require('express');
-var app = express();
+const path = require('path');
 
 module.exports.route = (app) => {
-	app.use(header);
-	app.get('/', (req,res,next)=>{res.write('Base Page'); next();});
-	app.use(footer);
+	app.get('/css/:css.css',css);
+	app.get('/js/:js.css',js);
+
+	app.get('/', [header,navbar,(req,res,next)=>{res.write('Base Page '.repeat(1000)); next();},bottomNavbar,footer]);
+}
+
+function css(req,res,next){
+	res.sendFile(path.resolve(__dirname+"/../common/css/"+req.params.css+".css"), 'utf8');
+}
+
+function js(req,res,next){
+	res.sendFile(path.resolve(__dirname+"/../common/js/"+req.params.js+".js"), 'utf8');
 }
 
 function header(req,res,next){
-	html = fs.readFileSync(__dirname+"/../common/header.html",'utf8');
+	html = fs.readFileSync(path.resolve(__dirname+"/../common/views/header.html"),'utf8');
+	res.write(html);
+	next();
+}
+
+function navbar(req,res,next){
+	html = fs.readFileSync(path.resolve(__dirname+"/../common/views/navbar.html"),'utf8');
+	res.write(html);
+	next();
+}
+
+function bottomNavbar(req,res,next){
+	html = fs.readFileSync(path.resolve(__dirname+"/../common/views/bottomNavbar.html"),'utf8');
 	res.write(html);
 	next();
 }
 
 function footer(req,res,next){
-	html = fs.readFileSync(__dirname+"/../common/footer.html",'utf8');
+	html = fs.readFileSync(path.resolve(__dirname+"/../common/views/footer.html"),'utf8');	
 	res.write(html);
 	res.end();
 }
